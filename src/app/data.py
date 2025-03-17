c55b47939205cc8620e9e97bec0b3e2aa155f8cd
Jonathan D. Baker <jonathan@ardent.tech>
2025-03-17 10:28:19 -0600

    flesh out unit tests

--- src/app/data.py
+++ src/app/data.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import csv
 from abc import ABC, abstractmethod
-from typing import List, Dict, Self
+from typing import List, Dict
 
 from app.domain import Book
 
@@ -59,11 +59,17 @@ class BookRepo:
     def __init__(self, data_source: DataSource):
         self._data_source = data_source
 
-    def fetch_all(self, params: BookParams) -> List[Book]:
-        if params.year is not None:
-            return [d for d in self._data_source.get_all() if d.year == params.year]
+    def fetch_all(self, params: BookParams | None = None) -> List[Book]:
+        if params is not None:
+            return self._fetch_all_with_params(params)
         else:
             return self._data_source.get_all()
 
     def fetch_one(self, id_: int) -> Book | None:
         return self._data_source.get_one(id_)
+
+    def _fetch_all_with_params(self, params: BookParams) -> List[Book]:
+        if params.year is not None:
+            return [d for d in self._data_source.get_all() if d.year == params.year]
+        else:
+            return self._data_source.get_all()
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,35 @@
-from app.data import BookParams
+import random
+from typing import List, Dict
+
+import factory
+
+from app.data import BookParams, DataSource, BookRepo
+from app.domain import Book
+
+
+class MemDataSource(DataSource):
+
+    def __init__(self):
+        self._data: Dict[int, Book] = dict()
+
+    def add_book(self, book: Book):
+        self._data[book.id] = book
+
+    def get_all(self) -> List[Book]:
+        return list(self._data.values())
+
+    def get_one(self, id_: int) -> Book | None:
+        return self._data.get(id_)
+
+
+class BookFactory(factory.Factory):
+    class Meta:
+        model = Book
+
+    author = factory.Faker("name")
+    id = factory.Sequence(lambda n: n)
+    title = factory.Faker('sentence', nb_words=4)
+    year = random.choice(range(2020, 2024))
 
 
 def test_book_params_from_dict():
@@ -8,24 +39,47 @@ def test_book_params_from_dict():
 
 
 def test_book_repo_fetch_all():
-    pass
+    data_source = MemDataSource()
+
+    count = 4
+    for _ in range(count):
+        data_source.add_book(BookFactory.build())
+
+    repo = BookRepo(data_source)
+    books = repo.fetch_all()
+    assert len(books) == count
 
 
 def test_book_repo_fetch_all_filter_year():
-    pass
+    data_source = MemDataSource()
+
+    count_a = 2
+    for _ in range(count_a):
+        data_source.add_book(BookFactory.build(year=2023))
+
+    count_b = 2
+    for _ in range(count_b):
+        data_source.add_book(BookFactory.build(year=2024))
+
+    repo = BookRepo(data_source)
+    params = BookParams(year=2024)
+    books = repo.fetch_all(params)
+    assert len(books) == count_b
 
 
 def test_book_repo_fetch_one_ok():
-    pass
+    id_ = 8
+    title_ = "testing"
+    data_source = MemDataSource()
+    data_source.add_book(BookFactory.build(id=id_, title=title_))
+
+    repo = BookRepo(data_source)
+    book = repo.fetch_one(id_)
+    assert book.title == title_
 
 
 def test_book_repo_fetch_one_none():
-    pass
-
-
-def test_csv_data_source_get_one_ok():
-    pass
-
-
-def test_csv_data_source_get_one_none():
-    pass
+    data_source = MemDataSource()
+    repo = BookRepo(data_source)
+    book = repo.fetch_one(16)
+    assert book is None
